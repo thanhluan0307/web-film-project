d240c33156a8fc007a64a7c881387d9d0d7af4aa
Trungbeo204 <trungbeo2042001@gmail.com>
2022-11-09 14:46:01 +0700

    login1

--- src/page/Login/login.jsx
+++ src/page/Login/login.jsx
@@ -8,13 +8,18 @@ const cx=classnames.bind(styles)
 function Login() {
 
 
+    // const [Username , setUsername]= useState('/^[a-zA-Z]{2,}$/');
+    // const [Email , setEmail]= useState('/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/');
+    // const [Password , setPassword]= useState('/^[a-zA-Z0-9!@#\$%\^\&*\)\(+=._-]{6,}$/');
+    const [booleanInp , setBooleanInp] = useState([false])
 
-    
+  
 
     const [count , setCount] = useState(0)
-    const [inpLogIn , setInpLogIn] = useState('')
-    const [inpSignUp , setInpSignUp] = useState('')
-
+    const [inpUserName , setInpUserName] = useState('')
+    const [inpPass , setInpPass] = useState('')
+    const [inpEmail , setInpEmail] = useState('')
+    const [inpConfirmPass , setInpConfirmPass] = useState('')
 
     function toSignUp(){
         setCount(count+1)
@@ -22,7 +27,20 @@ function Login() {
     function toLogIn(){
         setCount(count+1)
     }
+    const checkInp = () =>{
+        const regUsername = /^[a-zA-Z]{2,}$/
+        const regEmail = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
+        const regPass = /^[a-zA-Z0-9!@#\$%\^\&*\)\(+=._-]{6,}$/
 
+        if (!regUsername.test(inpUserName)) {
+            setBooleanInp(true)
+        }else{
+            setBooleanInp(false)
+        }
+        if (!regPass.test(inpPass)) {
+            
+        }
+    }
 
 
     return ( 
@@ -31,9 +49,9 @@ function Login() {
             <form style={{display:(count%2===0) ? "none" : "block"}} className={cx('form-login')}>
                 <h1>Login</h1>
                 
-                <input className={cx('inp-login')} value={inpLogIn} onChange={(e)=>setInpLogIn(e.target.value)} onInput={checkInp} type="text" placeholder='username'/> <br />
-                <p className={cx('err-msg' ,'hidden')}>*sai form rồi lè ^.^</p>
-                <input className={cx('inp-login')} value={inpLogIn} onChange={(e)=>setInpLogIn(e.target.value)} onInput={checkInp} type="password" placeholder='password'/> <br /> 
+                <input className={cx('inp-login')} value={inpUserName} onChange={(e)=>setInpUserName(e.target.value)} onInput={checkInp} type="text" placeholder='username'/> <br />
+                <p className={cx((booleanInp)?'err-msg' : 'hidden')}>*sai form rồi lè ^.^</p>
+                <input className={cx('inp-login')} value={inpPass} onChange={(e)=>setInpPass(e.target.value)} onInput={checkInp} type="password" placeholder='password'/> <br /> 
                 <p className={cx('err-msg' ,'hidden')}>*sai form rồi lè ^.^</p>
                 <div style={{textAlign:'right' , marginRight:"50px"}}>
 
@@ -48,13 +66,13 @@ function Login() {
 
             <form style={{display:(count%2===1) ? "none" : "block"}} className={cx('form-signup')}>
                 <h1>SignUp</h1>
-                <input className={cx('inp-SignUp')} value={inpSignUp} onChange={(e)=>setInpSignUp(e.target.value)} onInput={checkInp} type="text" placeholder='username'/> <br />
+                <input className={cx('inp-SignUp')} value={inpUserName} onChange={(e)=>setInpUserName(e.target.value)} onInput={checkInp} type="text" placeholder='username'/> <br />
                 <p className={cx('err-msg' ,'hidden')}>*sai form rồi lè ^.^</p>
-                <input className={cx('inp-SignUp')} value={inpSignUp} onChange={(e)=>setInpSignUp(e.target.value)} onInput={checkInp} type="email" placeholder='email'/> <br />
+                <input className={cx('inp-SignUp')} value={inpEmail} onChange={(e)=>setInpEmail(e.target.value)} onInput={checkInp} type="email" placeholder='email'/> <br />
                 <p className={cx('err-msg' ,'hidden')}>*sai form rồi lè ^.^</p>
-                <input className={cx('inp-SignUp')} value={inpSignUp} onChange={(e)=>setInpSignUp(e.target.value)} onInput={checkInp} type="password" placeholder='password'/> <br />
+                <input className={cx('inp-SignUp')} value={inpPass} onChange={(e)=>setInpPass(e.target.value)} onInput={checkInp} type="password" placeholder='password'/> <br />
                 <p className={cx('err-msg' ,'hidden')}>*sai form rồi lè ^.^</p>
-                <input className={cx('inp-SignUp')} value={inpSignUp} onChange={(e)=>setInpSignUp(e.target.value)} onInput={checkInp} type="password" placeholder='confirm password'/> <br />
+                <input className={cx('inp-SignUp')} value={inpConfirmPass} onChange={(e)=>setInpConfirmPass(e.target.value)} onInput={checkInp} type="password" placeholder='confirm password'/> <br />
                 <p className={cx('err-msg' ,'hidden')}>*sai form rồi lè ^.^</p>
                 <button className={cx('bnt-signup')}>
                     SignUp
