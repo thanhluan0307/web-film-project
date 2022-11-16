536c2d1735d5ab99b184ac1c0cf308ff6afd163e
thanhluan0307 <thanhluan030797@gmail.com>
2022-11-16 18:08:44 +0700

    update

--- src/page/Category/category.jsx
+++ src/page/Category/category.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import { useEffect, useState } from "react";
 import { useParams, useSearchParams } from "react-router-dom";
 import classNames from "classnames/bind";
