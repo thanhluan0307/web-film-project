dbd571680a5f4812e7dabc0f9791224b02862826
thanhluan0307 <thanhluan030797@gmail.com>
2022-11-20 14:33:50 +0700

    fix product

--- src/components/Product/product.jsx
+++ src/components/Product/product.jsx
@@ -1,32 +1,17 @@
-import { faCartShopping, faHeart } from "@fortawesome/free-solid-svg-icons";
+import { faCartShopping, faEye } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import classNames from "classnames/bind";
 import { Link } from "react-router-dom";
-import {addProduct,deleteProduct} from "~/reducer/favourite"
-import {useDispatch} from "react-redux"
 import styles from "./product.module.scss"
-import { useState } from "react";
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/blur.css';
 const cx = classNames.bind(styles)
 
 function Product({data}) {
-    const [check, setCheck] = useState(true)
-    const dispatch = useDispatch()
-    const addFavorite = () => {
-        const action = addProduct(data)
-        setCheck(false)
-        dispatch(action)
-    }
-    const removeFavorite = () => {
-        const action = deleteProduct(data) 
-        setCheck(true)
-        dispatch(action)
-    }
+  
   
     return (
         <div className={cx("wrapper")}>
-
             <div>
                 <Link to={`/product/${data._id}`}>
                     <LazyLoadImage
@@ -46,15 +31,11 @@ function Product({data}) {
             </div>
             <div className={cx('action')}>
                 <p><FontAwesomeIcon className={cx("icon-action")} icon={faCartShopping}/>Thêm </p>
-                    { check ?
-                        <p onClick={addFavorite} className={cx("link-product")}>
-                            <FontAwesomeIcon className={cx("icon-action")} icon={faHeart}/>Thích
-                        </p>
-                        :
-                        <p onClick={removeFavorite} className={cx("link-product")}>
-                            <FontAwesomeIcon className={cx("icon-action")} icon={faHeart}/>Bỏ Thích
-                        </p>
-                    }
+                <p className={cx("link-product")}>
+                    <Link to={`/product/${data._id}`} >
+                        <FontAwesomeIcon className={cx("icon-action")} icon={faEye}/>Xem
+                    </Link>
+                </p>      
             </div>
 
         </div>
