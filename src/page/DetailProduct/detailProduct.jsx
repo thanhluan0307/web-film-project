b0fd4bc4fdae3f72b49c87149ef3fad628287b9b
TranThiThuyDung <dungtrantt2k@gmail.com>
2022-11-22 16:35:19 +0700

    edit

--- src/page/DetailProduct/detailProduct.jsx
+++ src/page/DetailProduct/detailProduct.jsx
@@ -23,6 +23,7 @@ import { useDispatch } from "react-redux";
 import Alert from '~/components/Alert/alert';
 import { Link } from 'react-router-dom';
 import Modal from "~/components/Modal/modal";
+import logo from "~/assets/images/b5.png"
 
 const cx = classNames.bind(styles)
 var clone = [{
